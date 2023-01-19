4e34f93c3863eae37eb2eec48686c976f51d6312
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-01-19 21:40:07 +0800

    random algo feature

--- src/features/customAlgo/customAlgo/CustomAlgo.js
+++ src/features/customAlgo/customAlgo/CustomAlgo.js
@@ -5,7 +5,8 @@ import {
     setAlgoLineState,
     setCodeInput,
     addCodeInputToTypedAlgoOutput,
-    setTypedAlgoOutput
+    setTypedAlgoOutput,
+    setCodeSubmitted
 } from '../customAlgoSlice';
 
 export function CustomAlgo() {
@@ -15,12 +16,15 @@ export function CustomAlgo() {
     const typedAlgoOutput = customAlgoObject.typedAlgoOutput;
     const algoLineState = customAlgoObject.algoLineState;
     const codeInput = customAlgoObject.codeInput;
+    const presetAlgos = customAlgoObject.presetAlgos;
+    const codeSubmitted = customAlgoObject.codeSubmitted;
     const dispatch = useDispatch();
 
     function handleAnswerInputChange(e) {
         dispatch(setTypedAlgoOutput({ typedAlgoOutput: [] }));
         dispatch(setCodeInput({ codeInput: '' }));
         const codeSubmitted = e.target.value
+        dispatch(setCodeSubmitted({ codeSubmitted }))
         const AllLinesOfCode = codeSubmitted.split("\n");
         for (let i = 0; i < AllLinesOfCode.length; i++) {
             // remove all spaces including leading and trailing spaces and tabs
@@ -40,11 +44,12 @@ export function CustomAlgo() {
 
     function handleCodeInputChange(e) {
         if (customAlgoInput.length > 0) {
-            const comparisonCode = customAlgoInput[typedAlgoOutput.length];
+            const comparisonCodeUnclean = customAlgoInput[typedAlgoOutput.length];
             let val = e.target.value;
             dispatch(setCodeInput({ codeInput: val }))
             let codeInputVal = val.replace(/\s/g, '');
-            console.log(codeInputVal, comparisonCode)
+            let comparisonCode = comparisonCodeUnclean.replace(/\s/g, '');
+            console.log('code inputted is ', codeInputVal, 'comparison is ', comparisonCode)
             if (comparisonCode.substring(0, codeInputVal.length) === codeInputVal && codeInputVal !== comparisonCode) {
                 dispatch(setAlgoLineState({ algoLineState: 'semi' }))
             } else if (comparisonCode === codeInputVal) {
@@ -58,10 +63,35 @@ export function CustomAlgo() {
     function handleCodeInputKeyDown(e) {
         if (e.key === 'Enter' && algoLineState === 'correct') {
             dispatch(addCodeInputToTypedAlgoOutput())
-            if(typedAlgoOutput.length === customAlgoInput.length-1) {
+            if (typedAlgoOutput.length === customAlgoInput.length - 1) {
                 alert('You have completed the custom algo!')
             }
         }
+    }
+
+    function handleRandomiseAlgo(e) {
+        // generate random number between 0 and length of presetAlgos
+        const randomIndex = Math.floor(Math.random() * presetAlgos.length);
+        const randomAlgo = presetAlgos[randomIndex];
+        // create copy of randomAlgo
+        const randomAlgoCopy = [...randomAlgo];
+        // remove all spaces including leading and trailing spaces and tabs
+        let codeSubmittedString = ''
+        for (let i = 0; i < randomAlgoCopy.length; i++) {
+            // add each line of code to codeSubmittedString with a new line character
+            codeSubmittedString += randomAlgoCopy[i] + '\n';
+            randomAlgoCopy[i] = randomAlgoCopy[i].replace(/\s/g, '');
+        }
+        // remove empty string from array
+        for (let i = 0; i < randomAlgoCopy.length; i += 1) {
+            if (randomAlgoCopy[i] === "") {
+                randomAlgoCopy.splice(i, 1);
+            }
+        }
+
+        console.log('cleaned random algo', randomAlgoCopy)
+        dispatch(setCustomAlgoInput({ customAlgoInput: randomAlgo }))
+        dispatch(setCodeSubmitted({ codeSubmitted: codeSubmittedString }));
     }
 
     return (
@@ -76,9 +106,10 @@ export function CustomAlgo() {
                 })}
             </div>
             <h2>Custom Algo Learning Feature</h2>
+            <div className='button-div' onClick={handleRandomiseAlgo}>Randomise!</div>
             <input type="text" className={`code-input-${algoLineState}`} id="code-input" onChange={handleCodeInputChange} onKeyDown={handleCodeInputKeyDown} value={codeInput} />
             <h3> Create algo answer </h3>
-            <textarea rows={customAlgoInput.length + 1} cols="120" id="answer-input" onChange={handleAnswerInputChange} />
+            <textarea rows={customAlgoInput.length + 1} cols="120" id="answer-input" onChange={handleAnswerInputChange} value={codeSubmitted} />
         </div>
     );
 }
--- src/features/customAlgo/customAlgoSlice.js
+++ src/features/customAlgo/customAlgoSlice.js
@@ -5,8 +5,43 @@ export const customAlgoSlice = createSlice({
     initialState: {
         customAlgoInput: [],
         typedAlgoOutput: [],
+        codeSubmitted: '',
         algoLineState: 'semi',
-        codeInput: ''
+        codeInput: '',
+        presetAlgos: [[
+            'function bubbleSort(arr) {',
+            '    for (let i = 0; i < arr.length; i++) {',
+            '        for (let j = 0; j < arr.length; j++) {',
+            '            if (arr[j] > arr[j + 1]) {',
+            '                let temp = arr[j];',
+            '                arr[j] = arr[j + 1];',
+            '                arr[j + 1] = temp;',
+            '            }',
+            '        }',
+            '    }',
+            '    return arr;',
+            '}'
+        ], [
+            'function mergeSort(arr) {',
+            '    if (arr.length <= 1) return arr;',
+            '    let mid = Math.floor(arr.length / 2);',
+            '    let left = mergeSort(arr.slice(0, mid));',
+            '    let right = mergeSort(arr.slice(mid));',
+            '    return merge(left, right);',
+            '}',
+            '',
+            'function merge(left, right) {',
+            '    let merged = [];',
+            '    while (left.length && right.length) {',
+            '        if (left[0] < right[0]) {',
+            '            merged.push(left.shift());',
+            '        } else {',
+            '            merged.push(right.shift());',
+            '        }',
+            '    }',
+            '    return [...merged, ...left, ...right];',
+            '}'
+        ]]
     },
     reducers: {
         setCustomAlgoInput: (state, action) => {
@@ -24,15 +59,19 @@ export const customAlgoSlice = createSlice({
         },
         setTypedAlgoOutput(state, action) {
             state.typedAlgoOutput = action.payload.typedAlgoOutput;
+        },
+        setCodeSubmitted(state, action) {
+            state.codeSubmitted = action.payload.codeSubmitted;
         }
     }
 });
 
 export const {
     setCustomAlgoInput,
-    setAlgoLineState, 
-    setCodeInput, 
-    addCodeInputToTypedAlgoOutput, 
-    setTypedAlgoOutput 
+    setAlgoLineState,
+    setCodeInput,
+    addCodeInputToTypedAlgoOutput,
+    setTypedAlgoOutput,
+    setCodeSubmitted
 } = customAlgoSlice.actions;
 export default customAlgoSlice.reducer;
