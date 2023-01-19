5037ee33e3b2a951a870828ae6fc824961400365
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-01-19 20:56:15 +0800

    duplicate functionality

--- src/features/customAlgo/customAlgo/CustomAlgo.js
+++ src/features/customAlgo/customAlgo/CustomAlgo.js
@@ -1,40 +1,78 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {setCustomAlgoInput} from '../customAlgoSlice';
+import {
+    setCustomAlgoInput,
+    setAlgoLineState,
+    setCodeInput,
+    addCodeInputToTypedAlgoOutput
+} from '../customAlgoSlice';
 
 export function CustomAlgo() {
 
-  const customAlgoObject = useSelector((state) => state.customAlgo);
-  const customAlgoInput = customAlgoObject.customAlgoInput;
-  const dispatch = useDispatch();
+    const customAlgoObject = useSelector((state) => state.customAlgo);
+    const customAlgoInput = customAlgoObject.customAlgoInput;
+    const typedAlgoOutput = customAlgoObject.typedAlgoOutput;
+    const algoLineState = customAlgoObject.algoLineState;
+    const codeInput = customAlgoObject.codeInput;
+    const dispatch = useDispatch();
 
-  function handleAnswerInputChange(e) {
-    const codeSubmitted = e.target.value
-    const AllLinesOfCode = codeSubmitted.split("\n");
-    for (let i = 0; i < AllLinesOfCode.length; i++) {
-        // remove all spaces including leading and trailing spaces and tabs
-        AllLinesOfCode[i] = AllLinesOfCode[i].replace(/\s/g, '');
+    function handleAnswerInputChange(e) {
+        const codeSubmitted = e.target.value
+        const AllLinesOfCode = codeSubmitted.split("\n");
+        for (let i = 0; i < AllLinesOfCode.length; i++) {
+            // remove all spaces including leading and trailing spaces and tabs
+            AllLinesOfCode[i] = AllLinesOfCode[i].replace(/\s/g, '');
+        }
+
+        // remove empty string from array
+        for (let i = 0; i < AllLinesOfCode.length; i += 1) {
+            if (AllLinesOfCode[i] === "") {
+                AllLinesOfCode.splice(i, 1);
+            }
+        }
+
+        const customAlgoInput = AllLinesOfCode;
+        dispatch(setCustomAlgoInput({ customAlgoInput }))
     }
 
-    // remove empty string from array
-    for (let i = 0; i < AllLinesOfCode.length; i += 1) {
-        if (AllLinesOfCode[i] === "") {
-            AllLinesOfCode.splice(i, 1);
+    function handleCodeInputChange(e) {
+        if (customAlgoInput.length > 0) {
+            const comparisonCode = customAlgoInput[typedAlgoOutput.length];
+            let val = e.target.value;
+            dispatch(setCodeInput({ codeInput: val }))
+            let codeInputVal = val.replace(/\s/g, '');
+            console.log(codeInputVal, comparisonCode)
+            if (comparisonCode.substring(0, codeInputVal.length) === codeInputVal && codeInputVal !== comparisonCode) {
+                dispatch(setAlgoLineState({ algoLineState: 'semi' }))
+            } else if (comparisonCode === codeInputVal) {
+                dispatch(setAlgoLineState({ algoLineState: 'correct' }))
+            } else {
+                dispatch(setAlgoLineState({ algoLineState: 'incorrect' }))
+            }
         }
     }
 
-    const answer = AllLinesOfCode;
-    console.log('new answers array', answer);
-    dispatch(setCustomAlgoInput({customAlgoInput: answer}))
-  }
+    function handleCodeInputKeyDown(e) {
+        if (e.key === 'Enter' && algoLineState === 'correct') {
+            dispatch(addCodeInputToTypedAlgoOutput())
+        }
+    }
 
-  return (
-    console.log(customAlgoInput),
-    <div>
-      <h2>Custom Algo Feature</h2>
-      <input type="text" className="code-input" id="code-input" />
-      <h3> Create algo answer </h3>
-      <textarea rows={customAlgoInput.length+1} cols="120" id="answer-input" onChange={handleAnswerInputChange}/>
-    </div>
-  );
+    return (
+        <div>
+            <div className='answers-typed'>
+                {typedAlgoOutput.map((line, index) => {
+                    return (
+                        <div key={index}>
+                            {line}
+                        </div>
+                    )
+                })}
+            </div>
+            <h2>Custom Algo Feature</h2>
+            <input type="text" className={`code-input-${algoLineState}`} id="code-input" onChange={handleCodeInputChange} onKeyDown={handleCodeInputKeyDown} value={codeInput} />
+            <h3> Create algo answer </h3>
+            <textarea rows={customAlgoInput.length + 1} cols="120" id="answer-input" onChange={handleAnswerInputChange} />
+        </div>
+    );
 }
--- src/features/customAlgo/customAlgoSlice.js
+++ src/features/customAlgo/customAlgoSlice.js
@@ -2,17 +2,28 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export const customAlgoSlice = createSlice({
     name: 'customAlgo',
-    initialState:{
+    initialState: {
         customAlgoInput: [],
         typedAlgoOutput: [],
-        algoLineState: 'semi'
+        algoLineState: 'semi',
+        codeInput: ''
     },
-    reducers:{
+    reducers: {
         setCustomAlgoInput: (state, action) => {
             state.customAlgoInput = action.payload.customAlgoInput;
+        },
+        setAlgoLineState(state, action) {
+            state.algoLineState = action.payload.algoLineState;
+        },
+        setCodeInput(state, action) {
+            state.codeInput = action.payload.codeInput;
+        },
+        addCodeInputToTypedAlgoOutput(state) {
+            state.typedAlgoOutput.push(state.codeInput);
+            state.codeInput = '';
         }
     }
 });
 
-export const { setCustomAlgoInput } = customAlgoSlice.actions;
+export const { setCustomAlgoInput, setAlgoLineState, setCodeInput, addCodeInputToTypedAlgoOutput } = customAlgoSlice.actions;
 export default customAlgoSlice.reducer;
