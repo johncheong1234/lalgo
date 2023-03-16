72b59d0442f12222508414bd8bf04898bb9f9f67
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-03-16 09:16:38 +0800

    jwt decode install + user object

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import { CustomAlgo } from './features/customAlgo/customAlgo/CustomAlgo';
 import { SubmitAlgo } from './features/submitAlgo/submitAlgo/SubmitAlgo';
 import './CustomAlgo.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
 
 function App() {
 
   const handleLoginCallbackResponse = (response) => {
     console.log(response);
-
+    console.log(response.credential)
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject)
   };
 
   useEffect(() => {
@@ -19,11 +22,22 @@ function App() {
       client_id: "730704015839-bqolu76b7pilsg6pj895ntm2ntoutpju.apps.googleusercontent.com",
       callback: handleLoginCallbackResponse
     });
+
+    google.accounts.id.renderButton(
+      document.getElementById("loginButton"),
+      {
+        theme: "filled_blue",
+        size: "large",
+        text: "long",
+        type: "standard",
+      }
+    )
   }, []);
 
   return (
     <div>
       <h1>Learn Algos Fast Here</h1>
+      <div id="loginButton"></div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<CustomAlgo />} />
