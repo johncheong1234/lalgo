5b08478f2841ab1b69a6c0c3a2081b366cb3f301
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-03-16 09:11:26 +0800

    adding header+initial google object

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // import logo from './logo.svg';
 import { CustomAlgo } from './features/customAlgo/customAlgo/CustomAlgo';
 import { SubmitAlgo } from './features/submitAlgo/submitAlgo/SubmitAlgo';
@@ -7,14 +7,30 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
+  const handleLoginCallbackResponse = (response) => {
+    console.log(response);
+
+  };
+
+  useEffect(() => {
+
+    /*global google*/
+    google.accounts.id.initialize({
+      client_id: "730704015839-bqolu76b7pilsg6pj895ntm2ntoutpju.apps.googleusercontent.com",
+      callback: handleLoginCallbackResponse
+    });
+  }, []);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<CustomAlgo />} />
-        <Route path="/submit" element={<SubmitAlgo />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h1>Learn Algos Fast Here</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<CustomAlgo />} />
+          <Route path="/submit" element={<SubmitAlgo />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
