cbb74051ccc3f254ee8505b23b9a73d39f56e0b3
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-03-18 14:10:20 +0800

    remove comments

--- src/features/customAlgo/customAlgo/pastRecordsParent/PastRecordsParent.js
+++ src/features/customAlgo/customAlgo/pastRecordsParent/PastRecordsParent.js
@@ -1,11 +1,7 @@
 import React, {
     useEffect, useState
 } from "react";
-// import { useSelector, useDispatch } from "react-redux";
 import axios from 'axios';
-// import {
-//     setPastRecordsRender
-// } from '../../customAlgoSlice';
 
 export function PastRecordsParent(props) {
 
