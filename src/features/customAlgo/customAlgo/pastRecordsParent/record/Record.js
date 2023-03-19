0be1189705bc05240f3b4eba0b82268c38a71cab
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-03-19 10:03:37 +0800

    formating

--- src/features/customAlgo/customAlgo/pastRecordsParent/record/Record.js
+++ src/features/customAlgo/customAlgo/pastRecordsParent/record/Record.js
@@ -3,13 +3,16 @@ import React from 'react';
 export function Record(props) {
     const record = props.record;
     const timeTaken = record.endTime - record.startTime;
+    // format the start time
+    const startTime = new Date(record.startTime).toLocaleString();
+    const endTime = new Date(record.endTime).toLocaleString();
     return (
         <div className='record-card'>
             <p>
-                Start time: {record.startTime}
+                Start time: {startTime}
             </p>
             <p>
-                End time: {record.endTime}
+                End time: {endTime}
             </p>
             <p>
                 Time taken: {timeTaken / 1000} s
