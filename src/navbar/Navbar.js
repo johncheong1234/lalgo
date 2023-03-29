791aff75b0aa959a556811a96143fa5f1c26794b
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-03-29 13:42:17 +0800

    new items

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -63,23 +63,25 @@ export function Navbar() {
     return (
         <div className='navbar'>
             <div className='navbar-item'>
-                <a href='/'>Custom Algo</a>
+                <a href='/choose-line-game'>Choose Line Game</a>
             </div>
             <div className='navbar-item'>
-                <a href='/create-set'>Create Set</a>
+                <a href='/questions'>Questions</a>
             </div>
+            <div className='navbar-item'>
+                <a href='/'>Algo Training</a>
+            </div>
+            {/* <div className='navbar-item'>
+                <a href='/create-set'>Create Set</a>
+            </div> */}
             <div className='navbar-item'>
                 <a href='/set'>Set Training</a>
             </div>
             <div className='navbar-item'>
                 <a href='/submit'>Submit Algo</a>
             </div>
-            <div className='navbar-item'>
-                <a href='/questions'>Questions</a>
-            </div>
-            <div className='navbar-item'>
-                <a href='/choose-line-game'>Choose Line Game</a>
-            </div>
+            
+            
             {given_name ?
 
                 <div className='navbar-item' onClick={handleSignOut}>
