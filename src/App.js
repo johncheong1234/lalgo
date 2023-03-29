6e001bba719c85774299fc9dabecaa6d92917e45
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-03-29 13:31:13 +0800

    shifting sign in to navbar

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ import { Questions } from './features/questions/Questions';
 import { ChooseLineGame } from './features/chooseLineGame/ChooseLineGame';
 import { ChooseLineGameController } from './features/chooseLineGame/ChooseLineGameController';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import jwt_decode from "jwt-decode";
-import {
-  setUserObject
-} from './features/user/userSlice';
+// import jwt_decode from "jwt-decode";
+// import {
+//   setUserObject
+// } from './features/user/userSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
-import axios from 'axios';
+// import axios from 'axios';
 import { Analytics } from '@vercel/analytics/react';
 import { Navbar } from './navbar/Navbar';
 
@@ -25,55 +25,55 @@ function App() {
   const dispatch = useDispatch();
   const given_name = useSelector((state) => state.user.userObject.given_name);
 
-  const handleLoginCallbackResponse = (response) => {
-    console.log(response);
-    console.log(response.credential)
-    var userObject = jwt_decode(response.credential);
-    console.log(userObject)
-    dispatch(setUserObject({ userObject: userObject }));
-    // save userObject to local storage
-    window.localStorage.setItem('userObject', JSON.stringify(userObject));
-    const postUrl = 'https://ap-southeast-1.aws.data.mongodb-api.com/app/lalgo-ubstj/endpoint/upsert_user';
-    axios.post(postUrl, {
-      email: userObject.email,
-      name: userObject.name
-    })
-  };
+  // const handleLoginCallbackResponse = (response) => {
+  //   console.log(response);
+  //   console.log(response.credential)
+  //   var userObject = jwt_decode(response.credential);
+  //   console.log(userObject)
+  //   dispatch(setUserObject({ userObject: userObject }));
+  //   // save userObject to local storage
+  //   window.localStorage.setItem('userObject', JSON.stringify(userObject));
+  //   const postUrl = 'https://ap-southeast-1.aws.data.mongodb-api.com/app/lalgo-ubstj/endpoint/upsert_user';
+  //   axios.post(postUrl, {
+  //     email: userObject.email,
+  //     name: userObject.name
+  //   })
+  // };
 
-  useEffect(() => {
+  // useEffect(() => {
 
-    /*global google*/
-    google.accounts.id.initialize({
-      client_id: "730704015839-bqolu76b7pilsg6pj895ntm2ntoutpju.apps.googleusercontent.com",
-      callback: handleLoginCallbackResponse
-    });
+  //   /*global google*/
+  //   google.accounts.id.initialize({
+  //     client_id: "730704015839-bqolu76b7pilsg6pj895ntm2ntoutpju.apps.googleusercontent.com",
+  //     callback: handleLoginCallbackResponse
+  //   });
 
-    google.accounts.id.renderButton(
-      document.getElementById("loginButton"),
-      {
-        theme: "filled_blue",
-        size: "large",
-        text: "long",
-        type: "standard",
-      }
-    )
-  }, [given_name]);
+  //   google.accounts.id.renderButton(
+  //     document.getElementById("loginButton"),
+  //     {
+  //       theme: "filled_blue",
+  //       size: "large",
+  //       text: "long",
+  //       type: "standard",
+  //     }
+  //   )
+  // }, [given_name]);
 
-  useEffect(() => {
+  // useEffect(() => {
 
-    // get userObject from local storage
-    var userObject = JSON.parse(window.localStorage.getItem('userObject'));
-    if (userObject) {
-      dispatch(setUserObject({ userObject: userObject }));
-    }
+  //   // get userObject from local storage
+  //   var userObject = JSON.parse(window.localStorage.getItem('userObject'));
+  //   if (userObject) {
+  //     dispatch(setUserObject({ userObject: userObject }));
+  //   }
 
-  }, [])
+  // }, [])
 
-  function handleSignOut(e) {
-    e.preventDefault();
-    dispatch(setUserObject({ userObject: {} }));
-    window.localStorage.removeItem('userObject');
-  }
+  // function handleSignOut(e) {
+  //   e.preventDefault();
+  //   dispatch(setUserObject({ userObject: {} }));
+  //   window.localStorage.removeItem('userObject');
+  // }
 
   return (
     <div>
@@ -86,7 +86,7 @@ function App() {
           <Navbar />
         </div>
       </div>
-      {given_name ?
+      {/* {given_name ?
 
         <div className='sign-out' onClick={handleSignOut}>
           Sign out
@@ -97,7 +97,7 @@ function App() {
             width: '240px'
           }}>
         </div>
-        }
+        } */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<CustomAlgo />} />
--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,13 +1,64 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
     setUserObject
 } from '../features/user/userSlice';
 import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+import jwt_decode from "jwt-decode";
 
 export function Navbar() {
 
     const dispatch = useDispatch();
     const given_name = useSelector((state) => state.user.userObject.given_name);
+    const handleLoginCallbackResponse = (response) => {
+        console.log(response);
+        console.log(response.credential)
+        var userObject = jwt_decode(response.credential);
+        console.log(userObject)
+        dispatch(setUserObject({ userObject: userObject }));
+        // save userObject to local storage
+        window.localStorage.setItem('userObject', JSON.stringify(userObject));
+        const postUrl = 'https://ap-southeast-1.aws.data.mongodb-api.com/app/lalgo-ubstj/endpoint/upsert_user';
+        axios.post(postUrl, {
+            email: userObject.email,
+            name: userObject.name
+        })
+    };
+
+    useEffect(() => {
+
+        /*global google*/
+        google.accounts.id.initialize({
+            client_id: "730704015839-bqolu76b7pilsg6pj895ntm2ntoutpju.apps.googleusercontent.com",
+            callback: handleLoginCallbackResponse
+        });
+
+        google.accounts.id.renderButton(
+            document.getElementById("loginButton"),
+            {
+                theme: "filled_blue",
+                size: "large",
+                text: "long",
+                type: "standard",
+            }
+        )
+    }, [given_name]);
+
+    useEffect(() => {
+
+        // get userObject from local storage
+        var userObject = JSON.parse(window.localStorage.getItem('userObject'));
+        if (userObject) {
+            dispatch(setUserObject({ userObject: userObject }));
+        }
+
+    }, [])
+
+    function handleSignOut(e) {
+        e.preventDefault();
+        dispatch(setUserObject({ userObject: {} }));
+        window.localStorage.removeItem('userObject');
+    }
 
     return (
         <div className='navbar'>
@@ -29,6 +80,19 @@ export function Navbar() {
             <div className='navbar-item'>
                 <a href='/choose-line-game'>Choose Line Game</a>
             </div>
+            {given_name ?
+
+                <div className='navbar-item' onClick={handleSignOut}>
+                    Sign out
+                </div> :
+
+                <div id="loginButton"
+                    // style={{
+                    //     width: '240px'
+                    // }}
+                    >
+                </div>
+            }
         </div>
     )
 
