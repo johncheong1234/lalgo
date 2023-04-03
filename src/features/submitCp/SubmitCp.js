81cecc1ab957a96e50c0a9b59fa428ea8b05292d
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-04-03 10:56:12 +0800

    test case and algorithm fields

--- src/features/submitCp/SubmitCp.js
+++ src/features/submitCp/SubmitCp.js
@@ -14,7 +14,7 @@ export function SubmitCp() {
             <div style={{
                 display: 'flex',
             }}
-            className='submit-cp-header-container'
+                className='submit-cp-header-container'
             >
                 <span style={{
                     fontSize: '32px',
@@ -31,8 +31,42 @@ export function SubmitCp() {
                     borderRadius: '5px',
                     border: '1px solid black',
                 }}>
-                    Submit Test Case and Algorithm
+                    Submit
                 </span>
+            </div>
+            <div className='submit-cp-body-container'>
+                <span style={{
+                    fontSize: '24px',
+                    fontFamily: 'Trench'
+                }}>
+                    Test Case
+                </span>
+                <textarea style={{
+                    backgroundColor: '#1E333B',
+                    height: '100px',
+                    border: '1px solid white',
+                    borderRadius: '5px',
+                    fontFamily: 'Trench',
+                    fontSize: '18px',
+                    color: 'white',
+                    padding: '10px',
+                }} />
+                <span style={{
+                    fontSize: '24px',
+                    fontFamily: 'Trench'
+                }}>
+                    Algorithm
+                </span>
+                <textarea style={{
+                    backgroundColor: '#1E333B',
+                    height: '250px',
+                    border: '1px solid white',
+                    borderRadius: '5px',
+                    fontFamily: 'Trench',
+                    fontSize: '18px',
+                    color: 'white',
+                    padding: '10px',
+                }} />
             </div>
         </div>
     )
